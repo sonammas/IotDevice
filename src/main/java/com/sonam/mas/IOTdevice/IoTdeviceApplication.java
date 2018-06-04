02b6dbe8a190059b743431f481bcd4bc3ccc96e1
Sonam Masuriha <sonam.masuriha@powerhouse.nl>
2018-06-05 01:50:08 +0200

    setup

---
+++ src/main/java/com/sonam/mas/IOTdevice/IoTdeviceApplication.java
@@ -0,0 +1,12 @@
+package com.sonam.mas.IOTdevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTdeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoTdeviceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sonam/mas/IOTdevice/IoTdeviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sonam.mas.IOTdevice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IoTdeviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
